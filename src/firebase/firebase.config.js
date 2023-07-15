9d98c3d4ca3f211e9ac54a140c0e52b55332a79c
MDMahidul <mmbmahidul007@gmail.com>
2023-07-15 09:09:08 +0600

    created new react app and install all the dependencies and created routes and pages

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNEzHZtMJK1cS0TmXBTFyencQEZU_fTuk",
+  authDomain: "the-news-dragon-17331.firebaseapp.com",
+  projectId: "the-news-dragon-17331",
+  storageBucket: "the-news-dragon-17331.appspot.com",
+  messagingSenderId: "616438089861",
+  appId: "1:616438089861:web:944927a9a903199a3941ec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
